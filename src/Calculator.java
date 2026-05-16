125442a41171f8e99943aad83dc59d7597db3521
jmayta1984 <jmayta@pucp.edu.pe>
2026-05-15 19:30:20 -0500

    feat: add executeOperation method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,11 @@
+
 public class Calculator {
 
     public static void getMax(int a, int b) {
-        if (a > b ){
+        if (a > b) {
             System.out.println(a + " is greater than " + b + ".");
             return;
         }
-
 
         if (b > a) {
             System.out.println(b + " is greater than " + a + ".");
@@ -14,5 +14,17 @@ public class Calculator {
 
         System.err.println("Both numbers are equal.");
     }
-    
+
+    public static int executeOperation(int a, int b, char operator) {
+        int result = 0;
+        switch (operator) {
+            case '+' -> result = a + b;
+            case '-' -> result = a - b;
+            case '*' -> result = a * b;
+            case '/' -> result = a / b;
+            default -> throw new AssertionError();
+        }
+        return result;
+    }
+
 }
